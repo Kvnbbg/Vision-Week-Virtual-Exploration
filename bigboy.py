875dafcec8d9060d98530e6159862746830c1a30
Kevin Marville <kevinmarville@gmail.com>
2024-05-16 21:24:54 +0000

    refactor: Update file paths and remove unused code

--- bigboy.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Assuming you have a connection to your database established
-employees_df = pd.read_sql_query("SELECT name, salary FROM Employees", connection)
-
-# Access the name and salary columns
-names = employees_df["name"]
-salaries = employees_df["salary"]
