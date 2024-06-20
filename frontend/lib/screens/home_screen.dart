b267046fb8f7a5724494607302ce07e362853281
Kevin Marville <kevinmarville@gmail.com>
2024-06-20 10:18:32 +0000

    refactor: Update file paths and use relative paths for assets, scripts, and stylesheets

---
+++ frontend/lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EcranAccueil extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+          'Bienvenue à la Semaine Vision !\nExplorez le zoo et découvrez nos animaux !',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24.0)
+    ));
+  })
+}
