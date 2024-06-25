241f22cbf9d365863c1e4d2de34c86b30c0c7783
Kevin Marville <kvnbbg@gmail.com>
2024-06-25 05:09:27 +0200

    refactor: Update file paths and use relative paths for assets, scripts, and stylesheets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,115 @@
+/// The above code defines a Flutter application that uses Firebase Analytics to track user interactions
+/// and displays different screens based on the selected bottom navigation bar item.
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(SemaineVisionApp());
+}
+
+class SemaineVisionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vision Week',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: EcranPrincipal(),
+    );
+  }
+}
+
+class EcranPrincipal extends StatefulWidget {
+  @override
+  _EcranPrincipalState createState() => _EcranPrincipalState();
+}
+
+class _EcranPrincipalState extends State<EcranPrincipal> {
+  int _selectedIndex = 0;
+  FirebaseAnalytics analytics = FirebaseAnalytics();
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    EcranAccueil(),
+    EcranProfil(),
+    EcranCarte(),
+    EcranVideo(),
+    EcranVR(),
+    EcranParametres(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index);
+    });
+    analytics.logEvent(name: 'changement_onglet', parameters: {'index': index});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Semaine Vision'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Carte',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_library),
+            label: 'Vidéos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.vr_cardboard),
+            label: 'VR',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Paramètres',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vision Week',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
 import 'package:flutter/material.dart';
 
 void main() {
