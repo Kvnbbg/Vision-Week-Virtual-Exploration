1801c265435c095fabad8ba07ca9d03e1362cc17
Kevin Marville <kvnbbg@gmail.com>
2024-06-29 18:59:40 +0200

    refactor: Update file paths and use relative paths for assets, scripts, and stylesheets

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,38 @@
+/// The `HomeScreen` class in Dart represents the home screen of an app for exploring a virtual zoo
+/// during Vision Week.
+import 'package:flutter/material.dart';
+import 'map_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Accueil'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Bienvenue à la Semaine Vision !\nExplorez le zoo et découvrez nos animaux !',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapScreen()),
+                );
+              },
+              child: Text('Voir la Carte'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
