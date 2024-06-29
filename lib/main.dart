02f57e0ea748c4a4148f8cee5911ecc737bc5de4
Kevin Marville <kvnbbg@gmail.com>
2024-06-29 20:27:17 +0200

    refactor: Remove unused imports and clean up code formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:vision_week_virtual_exploration/screens/welcome_screen.dart';
-import 'package:vision_week_virtual_exploration/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
