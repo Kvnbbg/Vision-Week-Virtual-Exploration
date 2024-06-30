fce5db15e5c0eb056fd2b13d5a8670b1f4d732ba
Kevin Marville <kvnbbg@gmail.com>
2024-06-30 22:51:06 +0200

    refactor: Update vendor/composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kvnbbg/vision-week',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6943f9819d23d22821f0082af0fe84d68756bc8b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kvnbbg/vision-week' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6943f9819d23d22821f0082af0fe84d68756bc8b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
