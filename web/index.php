f3c47fe744caa0ae90b6acf0d857adba749afb1f
Kevin Marville <kevinmarville@gmail.com>
2024-07-05 00:04:10 +0200

    Set up PHP buildpack and MySQL configuration

--- web/index.php
+++ web/index.php
@@ -1,15 +1,19 @@
 <?php
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
+// Parse the JawsDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1);
 
-$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// Connect to the database
+$conn = new mysqli($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
 
+// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Serve the Flutter web build
 include_once("index.html");
 ?>
