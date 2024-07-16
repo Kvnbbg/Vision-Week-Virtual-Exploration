df7c9319e4a535064a9fd2f5c417b76daa8f9daf
Kevin Marville <kevinmarville@gmail.com>
2024-07-16 20:16:45 +0200

    chore: Update applicationId to "VisionWeekApp" in build.gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart' as p;
-import 'package:vision_week_virtual_exploration/navigation_screen.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/error_app.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
